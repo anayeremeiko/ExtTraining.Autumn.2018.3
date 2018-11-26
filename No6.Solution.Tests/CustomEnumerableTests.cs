b6e092c19cb791f060a0a55efab88623b71c64b9
Mintaka <spaceisperfect@gmail.com>
2018-11-26 11:50:39 +0300

    task 6

--- No6.Solution.Tests/CustomEnumerableTests.cs
+++ No6.Solution.Tests/CustomEnumerableTests.cs
@@ -2,31 +2,45 @@
 
 namespace No6.Solution.Tests
 {
+    using System;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+
     [TestFixture]
     public class CustomEnumerableTests
     {
         [Test]
         public void Generator_ForSequence1()
         {
-            int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
-
-            Assert.Inconclusive();
+            int[] expected = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+            var func = new Func<int, int, int>(
+                (beforePrevious, previous) => beforePrevious + previous);
+            var generator = new RangeGenerator<int>(1, 1, func, 10);
+            CollectionAssert.AreEqual(expected, generator.ToArray());
         }
 
         [Test]
         public void Generator_ForSequence2()
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
-
-            Assert.Inconclusive();
+            var func = new Func<int, int, int>(
+                (beforePrevious, previous) => (6 * previous - 8 * beforePrevious));
+            var generator = new RangeGenerator<int>(1, 2, func, 10);
+            CollectionAssert.AreEqual(expected, generator.ToArray());
         }
 
         [Test]
         public void Generator_ForSequence3()
         {
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
-
-            Assert.Inconclusive();
+            var func = new Func<double, double, double>(
+                (beforePrevious, previous) => (previous + (beforePrevious / previous)));
+            var generator = new RangeGenerator<double>(1, 2, func, 10);
+            int i = 0;
+            foreach (var number in generator)
+            {
+                Assert.AreEqual(expected[i++], number, 0.0000001);
+            }
         }
     }
 }
---
+++ No6/RangeGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace No6
+{
+    using System;
+    using System.Collections;
+
+    public class RangeGenerator<T> : IEnumerable<T>
+    {
+        private T first, second, current;
+
+        private readonly Func<T, T, T> rule;
+
+        private int length;
+
+        public RangeGenerator(T first, T second, Func<T, T, T> rule, int length)
+        {
+            this.first = first;
+            this.second = second;
+            this.rule = rule;
+            this.length = length;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.length; i++)
+            {
+                if (i == 0)
+                {
+                    yield return this.first;
+                }
+                else if (i == 1)
+                {
+                    yield return this.second;
+                }
+                else
+                {
+                    this.current = this.rule(this.first, this.second);
+                    this.first = this.second;
+                    this.second = this.current;
+                    yield return this.current;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
