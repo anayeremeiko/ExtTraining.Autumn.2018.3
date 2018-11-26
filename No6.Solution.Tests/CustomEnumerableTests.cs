c1cce8f0d615805661f78f690b35075f263d8fad
PavelShikolay <pavelshikolay@gmail.com>
2018-11-26 11:57:22 +0200

    No.6 task completed

--- No6.Solution.Tests/CustomEnumerableTests.cs
+++ No6.Solution.Tests/CustomEnumerableTests.cs
@@ -8,25 +8,64 @@ namespace No6.Solution.Tests
         [Test]
         public void Generator_ForSequence1()
         {
+            int quantity = 10;
+            int a = 1, b = 1;
+            int[] actual = new int[quantity];
+
+            var generator = SequenceGenerator.Generator(10, 1, 1, (x, y) => x + y);
+
+            int index = 0;
+
+            foreach(int item in generator)
+            {
+                actual[index++] = item;
+            }
+
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 
-            Assert.Inconclusive();
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [Test]
         public void Generator_ForSequence2()
         {
+            int quantity = 10;
+            int a = 1, b = 2;
+            int[] actual = new int[quantity];
+
+            var generator = SequenceGenerator.Generator(quantity, a, b, (x, y) => 6 * x - 8 * y);
+
+            int index = 0;
+
+            foreach (int item in generator)
+            {
+                actual[index++] = item;
+            }
+
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            Assert.Inconclusive();
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [Test]
         public void Generator_ForSequence3()
         {
+            int quantity = 10;
+            double a = 1, b = 2;
+            double[] actual = new double[quantity];
+
+            var generator = SequenceGenerator.Generator(quantity, a, b, (x, y) => x + y / x);
+
+            int index = 0;
+
+            foreach (double item in generator)
+            {
+                actual[index++] = item;
+            }
+
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            Assert.Inconclusive();
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
--- No6/SequenceGenerator.cs
+++ No6/SequenceGenerator.cs
@@ -6,6 +6,38 @@ namespace No6
 {
     public static class SequenceGenerator
     {
-        IEnumerable<T> 
+        /// <summary>
+        /// Generates sequence using specified member calculating algorithm
+        /// </summary>
+        /// <typeparam name="T">Generic type</typeparam>
+        /// <param name="quantity">Quantity</param>
+        /// <param name="a">A</param>
+        /// <param name="b">B</param>
+        /// <param name="func">Algorithm</param>
+        /// <returns>Enumerator for sequence</returns>
+        public static IEnumerable<T> Generator<T>(int quantity, T a, T b, Func<T, T, T> func)
+        {
+            if (quantity < 0)
+            {
+                throw new ArgumentException($"Value of {nameof(quantity)} must be non-negative");
+            }
+
+            return GeneratorCore(quantity, a, b, func);
+        }
+
+        private static IEnumerable<T> GeneratorCore<T>(int quantity, T a, T b, Func<T, T, T> func)
+        {
+            for (int i = 0; i < quantity; ++i)
+            {
+                yield return GetNextElement(i);
+            }
+
+            T GetNextElement(int index)
+            {
+                if (index == 0) return a;
+                if (index == 1) return b;
+                return func(GetNextElement(index - 1), GetNextElement(index - 2));
+            }
+        }
     }
 }
