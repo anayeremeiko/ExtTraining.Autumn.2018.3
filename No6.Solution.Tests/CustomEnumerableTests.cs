68f270bb39e7b5faeaa2638294ed0e7ab8379388
Allowner <se1999va@tut.by>
2018-11-26 11:52:40 +0300

    No6

--- No6.Solution.Tests/CustomEnumerableTests.cs
+++ No6.Solution.Tests/CustomEnumerableTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using No6.Solution;
+using NUnit.Framework;
+using System;
+using System.Linq;
 
 namespace No6.Solution.Tests
 {
@@ -9,24 +12,38 @@ namespace No6.Solution.Tests
         public void Generator_ForSequence1()
         {
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+            int[] calculated = Sequence.GenerateSequence<int>(1, 1, 10, 
+                new System.Func<int, int, int>((x, y) => x + y)).ToArray<int>();
 
-            Assert.Inconclusive();
+            Assert.AreEqual(expected, calculated);
         }
 
         [Test]
         public void Generator_ForSequence2()
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
+            int[] calculated = Sequence.GenerateSequence<int>(1, 2, 10,
+                            new System.Func<int, int, int>((x, y) => 6 * x - 8 * y)).ToArray<int>();
 
-            Assert.Inconclusive();
+            Assert.AreEqual(expected, calculated);
         }
 
         [Test]
         public void Generator_ForSequence3()
         {
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
+            double[] calculated = Sequence.GenerateSequence<double>(1, 2, 10,
+                            new System.Func<double, double, double>((x, y) => x + y / x)).ToArray<double>();
 
-            Assert.Inconclusive();
+            for (int i = 0; i < expected.Length; i++)
+            {
+                if (Math.Abs(expected[i] - calculated[i]) > 0.0000000001)
+                {
+                    Assert.Fail();
+                }
+            }
+
+            Assert.Pass();
         }
     }
 }
---
+++ No6/Sequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No6
+{
+    public static class Sequence
+    {
+        public static IEnumerable<T> GenerateSequence<T>(T first, T second, int count, Func<T,T,T> rule)
+        {
+            if (count < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            if (rule == null)
+            {
+                throw new ArgumentNullException(nameof(rule));
+            }
+
+            return Generate();
+
+            IEnumerable<T> Generate()
+            {
+                T previous = first, current = second;
+
+                for (int i = 0; i < count; i++)
+                {
+                    yield return previous;
+                    var temp = current;
+                    current = rule(current, previous);
+                    previous = temp;
+                }
+            }
+        }
+    }
+}
