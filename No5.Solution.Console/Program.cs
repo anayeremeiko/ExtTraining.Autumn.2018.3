b10e15945f0e674566d62022d76df42c713bba41
Allowner <se1999va@tut.by>
2018-11-26 11:17:24 +0300

    No 5

--- No5.Solution.Console/Program.cs
+++ No5.Solution.Console/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace No5.Solution.Console
+﻿using System.Collections.Generic;
+
+namespace No5.Solution.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            BoldText bold = new BoldText();
+            bold.Text = "bold";
+            Hyperlink hyperlink = new Hyperlink();
+            hyperlink.Text = "hyperlink";
+            PlainText plainText = new PlainText();
+            plainText.Text = "plainText";
+            List<DocumentPart> parts = new List<DocumentPart>() { bold, hyperlink, plainText };
+            Document document = new Document(parts);
+            System.Console.WriteLine("bold.ToHtml " + document.ToNewFormat(bold.ToHtml));
+            System.Console.WriteLine("bold.ToLaTeX " + document.ToNewFormat(bold.ToLaTeX));
+            System.Console.WriteLine("bold.ToPlainText " + document.ToNewFormat(bold.ToPlainText));
+            System.Console.WriteLine("hyperlink.ToHtml " + document.ToNewFormat(hyperlink.ToHtml));
+            System.Console.WriteLine("hyperlink.ToLaTeX " + document.ToNewFormat(hyperlink.ToLaTeX));
+            System.Console.WriteLine("hyperlink.ToPlainText " + document.ToNewFormat(hyperlink.ToPlainText));
+            System.Console.WriteLine("plainText.ToHtml " + document.ToNewFormat(plainText.ToHtml));
+            System.Console.WriteLine("plainText.ToLaTeX " + document.ToNewFormat(plainText.ToLaTeX));
+            System.Console.WriteLine("plainText.ToPlainText " + document.ToNewFormat(plainText.ToPlainText));
+            System.Console.ReadKey();
         }
     }
 }
---
+++ No5.Solution/Document.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No5.Solution
+{
+    public class Document
+    {
+        private readonly List<DocumentPart> parts;
+
+        public Document(IEnumerable<DocumentPart> parts)
+        {
+            if (parts == null)
+            {
+                throw new ArgumentNullException(nameof(parts));
+            }
+
+            this.parts = new List<DocumentPart>(parts);
+        }
+
+        public string ToNewFormat(Func<string> formatFunction)
+        {
+            string output = string.Empty;
+
+            foreach (DocumentPart part in this.parts)
+            {
+                output += formatFunction() + "\n";
+            }
+
+            return output;
+        }
+    }
+}
